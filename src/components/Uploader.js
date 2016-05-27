78848b60676f271202e8922692fdde340a7af90a
Zarema Khalilova <zarema.khalilova@gmail.com>
2016-05-27 17:24:56 +0300

    add destroy example

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -7,12 +7,44 @@ class Uploader extends Component {
     uploadcare.start({
       publicKey: "demopublickey"
     });
+    uploadcare.Widget("#Uploadcare2");
+    uploadcare.Widget("#Uploadcare3");
+  }
+
+  reset() {
+    uploadcare.reset();
+  }
+
+  initialize() {
+    uploadcare.initialize();
+  }
+
+  destroyAll() {
+    uploadcare.reset();
+
+    let widgetInputs = document.querySelectorAll("[id^=Uploadcare");
+    widgetInputs.forEach(input => {
+      (uploadcare.Widget(input)).destroy();
+    });
+  }
+
+  resurrectAll() {
+    uploadcare.initialize();
+    uploadcare.Widget("#Uploadcare2");
+    uploadcare.Widget("#Uploadcare3");
   }
 
   render() {
     return (
       <div>
-        <input type="text" role="uploadcare-uploader"/>
+        <input type="hidden" role="uploadcare-uploader"/>
+        <input type="hidden" id="Uploadcare2" data-images-only/>
+        <input type="hidden" id="Uploadcare3" data-multiple/>
+        <hr/>
+        <button onClick={this.reset}>reset</button>
+        <button onClick={this.initialize}>initialize</button>
+        <button onClick={this.destroyAll}>destroy all!</button>
+        <button onClick={this.resurrectAll}>resurrect all!</button>
       </div>
     )
   }
